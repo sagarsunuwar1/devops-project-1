a76617d385396f32457d71a901e15349b9f0822c
Sagar Sunuwar <sunuwars189@gmail.com>
2025-12-07 08:26:38 +0000

    Check status of apache2 service

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 echo "Demo scripts"
 apt udate
 apt install -y apache2
+
+if [ -f /var/run/apache2/apache2.pid ]
+; then
+echo "Apache2 is running"
+fi
 sudo systemctl start apache2
 sudo systemctl enable apache2
 sudo systemctl status apache2
